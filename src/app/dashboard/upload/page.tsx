7b7ebad8403c94c12f22e68efba05e26c5d6a313
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-27 00:30:04 +0530

    feat: Dashboard Page [WIP]

---
+++ src/app/dashboard/upload/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import { useState, useRef, useEffect } from "react";
+
+import { Button } from "primereact/button";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import secureLocalStorage from 'react-secure-storage';
+
+const NewProject = () => {
+
+}
+
+export default NewProject;
