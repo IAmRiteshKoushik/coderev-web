5271454e637b2b92cef6cd7be1ccc44d1aeeb857
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-26 12:05:13 +0530

    fix: Error handling and secureLocalStorage in OTP-verify page

--- src/app/register/verify/page.tsx
+++ src/app/register/verify/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { REGISTER_VERIFY_URL } from '../../utils/constants';
 import React, { useState, useRef, useEffect } from "react";
 import secureLocalStorage from "react-secure-storage";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-import 
 import { Toast } from "primereact/toast";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-blue/theme.css"
 
-export default function verifyPage(){
+import { REGISTER_VERIFY_URL } from '../../_utils/constants';
+
+export default function VerifyPage(): JSX.Element {
     
     const toast = useRef<Toast>(null);
 
@@ -19,25 +19,47 @@ export default function verifyPage(){
     const otpRegex = /^[0-9]{6}/;
     const isValidOtp = otp.length === 6 && otpRegex.test(otp.join());
 
-    const [registerEmail, setRegisterEmail] = useState("");
-    const [registerToken, setRegisterToken] = useState("");
+    const [registerEmail, setRegisterEmail] = useState(secureLocalStorage.getItem("registerEmail"));
+    const [registerToken, setRegisterToken] = useState(secureLocalStorage.getItem("registerToken"));
     const [loading, setLoading] = useState(false);
 
     const router = useRouter();
-
-    useEffect(() => {
-        setRegisterEmail(secureLocalStorage.getItem("registerEmail"));
-        setRegisterToken(secureLocalStorage.getItem("registerToken"));
-    }, []);
 
     const handleVerify = async (e: MouseEvent) => {
         e.preventDefault();
         const otpString = otp.join();
 
-
-
         try{
+            const response = await fetch(REGISTER_VERIFY_URL, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Authorization": "Bearer" + registerToken, 
+                },
+                body: JSON.stringify({
+                    otp: otpString
+                }),
+            });
 
+            const data = await response.json();
+
+            if(response.status === 200){
+                alertSuccess("Registration Successful", "OTP verified successfully");
+                secureLocalStorage.clear();
+                secureLocalStorage.setItem("userAccess", data["SECRET_TOKEN"]);
+                secureLocalStorage.setItem("currentUser", JSON.stringify({
+                    
+                }));
+                setTimeout(() => {
+                    router.replace("/dashboard");
+                }, 2000);
+            } else if (response.status === 500){
+                alertError("Oops!", "Something went wrong! Please try again later!");
+            } else if (data.messge !== undefined || data.message !== null ){
+                alertError("Registration Failed", data.message);
+            } else {
+                alertError("Oops!", "Something went wrong! Please try again later!");
+            }
         }catch(error){
             console.log(error);
             alertError('Oops!', 'Something went wrong! Please try again!');
