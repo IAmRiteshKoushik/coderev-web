25f9e3173e0704d7c9f6d439695572607cf36796
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-26 22:08:58 +0530

    feat: Registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,13 +3,16 @@
 import Link from "next/link";
 import { useRouter } from 'next/navigation';
 import { useState, useRef, useEffect } from 'react';
+import { z } from "zod";
+
 import { Toast } from "primereact/toast";
-import { SelectButton } from "primereact/selectbutton";
+
+import { Password } from "primereact/password";
 import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/lara-light-blue/theme.css"
 import secureLocalStorage from 'react-secure-storage';
 
-import { hashPassword } from "../_utils/hash";
+import hashPassword from "../_utils/hash";
 import { REGISTER_URL } from '../_utils/constants';
 
 const Register = () => {
@@ -23,22 +26,22 @@ const Register = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
+    const [userEmail, setUserEmail] = useState('');
     const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [userPassword, setUserPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
-
-    const [passwordMatchError, setPasswordMatchError] = useState(false);
     const [loading, setLoading] = useState(false);
-
-    // Regular expressions for matching
 
     const router = useRouter();
 
-    const isValid = () => {
-
-    }
-
+    // Validators
+    const isValidFirstName = z.string().min(1).max(50).safeParse(firstName);
+    const isValidLastName = z.string().min(1).max(50).safeParse(lastName);
+    const isValidEmail = z.string().min(5).max(320).email().safeParse(userEmail);
+    const isValidPassword = z.string().min(8).safeParse(userPassword);
+    const isValidConfirmPassword = z.string().min(8).max(50).safeParse(confirmPassword);
+    const isMatchingPassword = userPassword === confirmPassword;
+    
     const alertError = (summary: String, detail: String) => {
         toast.current?.show({
             severity: "error",
@@ -55,26 +58,36 @@ const Register = () => {
         });
     };
 
-  // const handleRegister = () => {
-  //   if (password !== confirmPassword) {
-  //     setPasswordMatchError(true);
-  //     return;
-  //   }
-  //   setPasswordMatchError(false);
-  //
-  //   console.log('Registering with:', firstName, lastName, email, username, password);
-  //   window.location.href = '/login';
-  // };
 
-    const handleRegister = async(e: MouseEvent) => {
+    const handleRegister = async(e: any) => {
         e.preventDefault();
 
-        if(!isValid) {
+        // Guard Clauses
+        if(!isValidFirstName.success) {
             alertError("Invalid Details", 
                 "Please check all the fields and try again.");
             return;
         }
-        setLoading(true);
+        if(!isValidLastName.success) {
+            alertError("Invalid Details", 
+                "Please check all the fields and try again.");
+            return;
+        }
+        if(!isValidEmail.success) {
+            alertError("Invalid Details", 
+                "Please check all the fields and try again.");
+            return;
+        }
+        if(!isValidPassword.success) {
+            alertError("Invalid Password", 
+                "Please check the password field and try again.");
+            return;
+        }
+        if(!isMatchingPassword) {
+            alertError("Confirm Password Error", 
+                "Both passwords do not seem to match!");
+            return;
+        }
 
         try {
             const response = await fetch(REGISTER_URL, {
@@ -87,8 +100,8 @@ const Register = () => {
                     "firstName" : firstName,
                     "lastName" : lastName,
                     "username" : username,
-                    "email" : email,
-                    "password" : hashPassword(password),
+                    "email" : userEmail,
+                    "password" : hashPassword(userPassword),
                 })
             });
 
@@ -98,7 +111,7 @@ const Register = () => {
                     "Redirection to OTP Verification...");
                 // console.log(data); JUST for checking
                 secureLocalStorage.setItem("registerToken", data["SECRET_TOKEN"]);
-                secureLocalStorage.setItem("registerEmail", email);
+                secureLocalStorage.setItem("registerEmail", userEmail);
 
                 setTimeout(() => {
                     router.push("/register/verify");
@@ -121,128 +134,109 @@ const Register = () => {
     }
 
   return (
-    <div className="bg-black min-h-screen flex items-center justify-center">
-      <div className="max-w-md w-full p-8 bg-black rounded-lg shadow-md border border-white space-y-4">
-        <h2 className="text-2xl font-semibold text-center">Register</h2>
-        <div className="grid grid-cols-2 gap-4">
-          <input
-            type="text"
-            placeholder="First Name"
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-green-500 text-black"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-green-500 text-black"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="email"
-            placeholder="Email"
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-green-500 text-black"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-green-500 text-black"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="relative">
-          <input
-            type={showPassword ? 'text' : 'password'}
-            placeholder="Password"
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-green-500 text-black"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button
-            className="absolute inset-y-0 right-0 flex items-center px-3 text-gray-500 focus:outline-none"
-            onClick={() => setShowPassword(!showPassword)}
-          >
-            {showPassword ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 4v.01M12 8V4v4zm0 8v4v-4zm-6 4h4-4zm12 0h4-4zm-8-8H4h4zm12 0h-4 4zm-8 0v-4 4zm0-8V4v4z"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-        
-        <div>
-    <input
-      type={showPassword ? "text" : "password"}
-      placeholder="Confirm Password"
-      className={`w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-green-500 text-black ${
-        passwordMatchError ? 'border-red-500' : ''
-      }`}
-      value={confirmPassword}
-      onChange={(e) => {
-        setConfirmPassword(e.target.value);
-        if (passwordMatchError) {
-          setPasswordMatchError(false);
-        }
-      }}
-    />
-    
-    {passwordMatchError && (
-      <p className="text-red-500 text-xs mt-1">Passwords do not match</p>
-    )}
-  </div>
-        <button
-          className="w-full bg-green-500 text-white py-2 rounded-md hover:bg-green-600 focus:outline-none"
-          onClick={handleRegister}
-        >
-          Register
-        </button>
-        <div className="text-center">
-          <p>
-            Already have an account? <a href="/login">Login</a>
-          </p>
-        </div>
-      </div>
-    </div>
+        <main className="bg-slate-100 min-h-screen flex items-center justify-center">
+            <Toast position="bottom-center" ref={toast} />
+            <div className="border-slate-400 border-2 p-10 rounded-md bg-slate-200">
+                {/* Heading */}
+                <h1 className="font-bold text-2xl p-2 text-center">Create Account</h1>
+                {/* Registration Form */}
+                <form className="space-y-6" onSubmit={handleRegister}>
+                    {/* First Name */}
+                    <div>
+                        <label>
+                            First Name
+                        </label>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder='Enter your First Name'
+                                onChange={(e) => setFirstName(e.target.value)}
+                                className={"block text-lg w-full rounded-md py-2 px-2 text-black shadow-sm ring-1 ring-inset ring-bGray placeholder:text-gray-400 sm:text-md sm:leading-6 !outline-none" +
+                                    (!isValidFirstName.success && firstName ? ' ring-red-500' : isValidFirstName.success && firstName ? ' ring-green-500' : ' ring-bGray')}
+                                required
+                            />
+                        </div>
+                    </div>
+                    {/* Last Name */}
+                    <div>
+                        <label>
+                            Last Name
+                        </label>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder='Enter your Last Name'
+                                onChange={(e) => setLastName(e.target.value.toLowerCase())}
+                                className={"block text-lg w-full rounded-md py-2 px-2 text-black shadow-sm ring-1 ring-inset ring-bGray placeholder:text-gray-400 sm:text-md sm:leading-6 !outline-none" +
+                                    (!isValidLastName.success && lastName ? ' ring-red-500' : isValidLastName.success && lastName ? ' ring-green-500' : ' ring-bGray')}
+                                required
+                            />
+                        </div>
+                    </div>
+                    {/* Email */}
+                    <div>
+                        <label>
+                            Email
+                        </label>
+                        <div>
+                            <input
+                                type="email"
+                                placeholder='Enter your Email'
+                                onChange={(e) => setUserEmail(e.target.value)}
+                                className={"block text-lg w-full rounded-md py-2 px-2 text-black shadow-sm ring-1 ring-inset ring-bGray placeholder:text-gray-400 sm:text-md sm:leading-6 !outline-none" +
+                                    (!isValidEmail.success && userEmail ? ' ring-red-500' : isValidEmail.success && userEmail ? ' ring-green-500' : ' ring-bGray')}
+                                required
+                            />
+                        </div>
+                    </div>
+                    {/* Password */}
+                    <div>
+                        <label>
+                           Password 
+                        </label>
+                        <div>
+                            <input
+                                type="password"
+                                placeholder='Enter your Password'
+                                onChange={(e) => setUserPassword(e.target.value)}
+                                className={"block text-lg w-full rounded-md py-2 px-2 text-black shadow-sm ring-1 ring-inset ring-bGray placeholder:text-gray-400 sm:text-md sm:leading-6 !outline-none" +
+                                    (!isValidPassword.success && userPassword ? ' ring-red-500' : isValidPassword.success && userPassword ? ' ring-green-500' : ' ring-bGray')}
+                                required
+                            />
+                        </div>
+                    </div>
+                    {/* Confirm Password */}
+                    <div>
+                        <label>
+                            Confirm Password
+                        </label>
+                        <div>
+                            <input
+                                type="password"
+                                placeholder='Confirm your password'
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                className={"block text-lg w-full rounded-md py-2 px-2 text-black shadow-sm ring-1 ring-inset ring-bGray placeholder:text-gray-400 sm:text-md sm:leading-6 !outline-none" +
+                                    (!isValidConfirmPassword.success && confirmPassword ? ' ring-red-500' : isValidConfirmPassword.success && confirmPassword ? ' ring-green-500' : ' ring-bGray')}
+                                required
+                            />
+                        </div>
+                    </div>
+
+                    {/* Already Have an Account ? */}
+                    <p className="mt-10 text-center text-md text-gray-500">
+                        {"Have an account already? "}
+                        <Link className="font-medium leading-6 text-blue-600 hover:underline" href="/login">Login</Link>
+                    </p>
+                    <div>
+                        <input
+                            value="Register"
+                            type="submit"
+                            className="w-full text-lg rounded-lg bg-black text-white p-2 cursor-pointer"
+                        ></input>
+                    </div>
+                </form>
+            </div>
+    </main>
   );
 };
 
