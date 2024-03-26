c6d84a6e7af11ee4567499b277a4f5ebac218ff8
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-03-26 12:44:40 +0530

    add: Password hashing mechanism, route-URLs

--- src/app/_utils/constants.ts
+++ src/app/_utils/constants.ts
@@ -1,1 +1,26 @@
-export const REGISTER_VERIFY_URL = "";
+const BASE_URL = "";
+
+// User Routes
+export const LOGIN_URL = BASE_URL + "/login";
+export const REGISTER_URL = BASE_URL + "/register";
+export const REGISTER_VERIFY_URL = BASE_URL + "/registerVerify";
+export const GET_PROFILE_URL = BASE_URL + "/getUserProfile";
+
+export const FORGOT_PASSWORD_URL = BASE_URL + "/forgotPassword"
+export const FORGOT_PASSWORD_VERIFY_URL = BASE_URL + "/resetPasswordVerify";
+export const RESET_PASSWORD_URL = BASE_URL + "";
+
+export const EDIT_PROFILE_URL = BASE_URL + "/editProfile";
+export const DELETE_ACCOUNT_URL = BASE_URL + "/deleteAccount";
+
+// Project Routes
+export const GET_ALL_PROJECTS_URL = BASE_URL + "/projects";
+export const GET_PROJECT_BY_ID_URL = BASE_URL + "/";
+export const GET_ALL_REVIEWS_URL = BASE_URL + "/";
+export const GET_REVIEW_BY_ID_URL = BASE_URL + "/";
+
+export const ADD_PROJECT_URL = BASE_URL + "/";
+export const EDIT_PROJECT_DETAILS_URL = BASE_URL + "/";
+export const DELETE_PROJECT_URL = BASE_URL + "/";
+
+// Further routes are to be added
