ab61781f621c4cfa87c457db66c3a7aee8adaddd
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-02 22:47:50 +0530

    fix: Localhost routes fixed

--- src/app/_utils/constants.ts
+++ src/app/_utils/constants.ts
@@ -1,26 +1,25 @@
-const BASE_URL = "";
+// Backend server URL
+const BASE_URL = "http://localhost:5000/api";
 
 // User Routes
-export const LOGIN_URL = BASE_URL + "/login";
-export const REGISTER_URL = BASE_URL + "/register";
-export const REGISTER_VERIFY_URL = BASE_URL + "/registerVerify";
-export const GET_PROFILE_URL = BASE_URL + "/getUserProfile";
-
-export const FORGOT_PASSWORD_URL = BASE_URL + "/forgotPassword"
-export const FORGOT_PASSWORD_VERIFY_URL = BASE_URL + "/resetPasswordVerify";
-export const RESET_PASSWORD_URL = BASE_URL + "";
-
-export const EDIT_PROFILE_URL = BASE_URL + "/editProfile";
-export const DELETE_ACCOUNT_URL = BASE_URL + "/deleteAccount";
+export const LOGIN_URL = BASE_URL + "/user/login";
+export const REGISTER_URL = BASE_URL + "/user/register";
+export const EDIT_PROFILE_URL = BASE_URL + "/user/edit-profile";
+export const DELETE_ACCOUNT_URL = BASE_URL + "/user/delete-profile";
 
 // Project Routes
-export const GET_ALL_PROJECTS_URL = BASE_URL + "/projects";
-export const GET_PROJECT_BY_ID_URL = BASE_URL + "/";
-export const GET_ALL_REVIEWS_URL = BASE_URL + "/";
-export const GET_REVIEW_BY_ID_URL = BASE_URL + "/";
+export const CREATE_PROJECT_URL = BASE_URL + "/project/create-project";
+export const ADD_FILE_URL = BASE_URL + "/project/add-files";
 
-export const ADD_PROJECT_URL = BASE_URL + "/";
-export const EDIT_PROJECT_DETAILS_URL = BASE_URL + "/";
-export const DELETE_PROJECT_URL = BASE_URL + "/";
+export const GET_ALL_PROJECTS_URL = BASE_URL + "/project/get-all-projects";
+export const GET_PROJECT_URL = BASE_URL + "/project/get-project/:project-id";
 
-// Further routes are to be added
+export const PROJECT_REVIEW_URL = BASE_URL + "/project/review/project-review";
+export const FILE_REVIEW_URL = BASE_URL + "/project/review/file-review";
+export const SNIPPET_REVIEW_URL = BASE_URL + "/project/review/snip-review";
+
+export const DELETE_PROJECT_URL = BASE_URL + "/project/delete-project/";
+export const DELETE_FILE_URL = BASE_URL + "/project/delete-file";
+export const DELETE_SNIP_URL = BASE_URL + "/project/delete-snip"
+
+// File Routes
--- src/app/_utils/hash.ts
+++ src/app/_utils/hash.ts
@@ -3,3 +3,4 @@ import crypto from "crypto";
 export default function hashPassword(password: string){
     return crypto.createHash("sha256").update(password).digest("hex");
 }
+
