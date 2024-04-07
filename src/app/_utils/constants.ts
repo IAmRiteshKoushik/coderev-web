93bc0186a08ed34111f3027c66a52fa66096fd49
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-07 10:47:05 +0530

    fix: constants.ts - removed redundant routes

--- src/app/_utils/constants.ts
+++ src/app/_utils/constants.ts
@@ -9,17 +9,10 @@ export const DELETE_ACCOUNT_URL = BASE_URL + "/user/delete-profile";
 
 // Project Routes
 export const CREATE_PROJECT_URL = BASE_URL + "/project/create-project";
-export const ADD_FILE_URL = BASE_URL + "/project/add-files";
-
 export const GET_ALL_PROJECTS_URL = BASE_URL + "/project/get-all-projects";
-export const GET_PROJECT_URL = BASE_URL + "/project/get-project/:project-id";
-
-export const PROJECT_REVIEW_URL = BASE_URL + "/project/review/project-review";
-export const FILE_REVIEW_URL = BASE_URL + "/project/review/file-review";
-export const SNIPPET_REVIEW_URL = BASE_URL + "/project/review/snip-review";
-
+export const GET_PROJECT_URL = BASE_URL + "/project/get-project";
+export const CREATE_PROJECT_REVIEW_URL = BASE_URL + "/project/review/project-review";
+export const GET_FILE_REVIEW_URL = BASE_URL + "/project/review/file-review";
+export const UPLOAD_TO_SERVER_URL = BASE_URL + "/project/upload-files";
 export const DELETE_PROJECT_URL = BASE_URL + "/project/delete-project/";
 export const DELETE_FILE_URL = BASE_URL + "/project/delete-file";
-export const DELETE_SNIP_URL = BASE_URL + "/project/delete-snip"
-
-// File Routes
