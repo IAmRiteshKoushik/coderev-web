41fa87ae056c2d7f58ad544fc55f733d61cee907
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-07 10:51:06 +0530

    fix: Removed empty components

---
+++ src/app/dashboard/_components/CodeViewer.tsx
@@ -0,0 +1,39 @@
+"use client";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { atomOneLight } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+interface CodeContent {
+    lines: string[],
+    filetype: "Python" | "JavaScript" | "Java" | "Markdown",
+    start: number, 
+    end: number
+}
+
+const CodeViewer = (props: CodeContent): JSX.Element => {
+
+    let highlightList: number[] = [];
+    for(let i = props.start; i < props.end + 1; i++){
+        highlightList.push(i);
+    }
+
+    return(
+        <div className='rounded-md'>
+            <SyntaxHighlighter language={props.filetype} style={atomOneLight} 
+                showLineNumbers
+                wrapLongLines
+                className="leading-5 text-sm"
+                lineProps={lineNumber => {
+                    let style = { display: "block", backgroundColor: "#ffffff" };
+                    if (highlightList.includes(lineNumber)){
+                        style.backgroundColor='#b3f2bb'
+                    }
+                    return { style };
+                }}
+            >
+                {props.lines.join("\n")}
+            </SyntaxHighlighter>
+        </div>
+    );
+}
+
+export default CodeViewer;
