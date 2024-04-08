2ee7240fca7c1d1b64b4454f7d966bce9b0b376b
Ritesh Koushik <riteshkoushik39@gmail.com>
2024-04-08 09:05:08 +0530

    new: Route for getting review

--- src/app/_utils/constants.ts
+++ src/app/_utils/constants.ts
@@ -5,14 +5,14 @@ const BASE_URL = "http://localhost:5000/api";
 export const LOGIN_URL = BASE_URL + "/user/login";
 export const REGISTER_URL = BASE_URL + "/user/register";
 export const EDIT_PROFILE_URL = BASE_URL + "/user/edit-profile";
-export const DELETE_ACCOUNT_URL = BASE_URL + "/user/delete-profile";
 
 // Project Routes
 export const CREATE_PROJECT_URL = BASE_URL + "/project/create-project";
 export const GET_ALL_PROJECTS_URL = BASE_URL + "/project/get-all-projects";
+
 export const GET_PROJECT_URL = BASE_URL + "/project/get-project";
 export const CREATE_PROJECT_REVIEW_URL = BASE_URL + "/project/review/project-review";
+
 export const GET_FILE_REVIEW_URL = BASE_URL + "/project/review/file-review";
-export const UPLOAD_TO_SERVER_URL = BASE_URL + "/project/upload-files";
-export const DELETE_PROJECT_URL = BASE_URL + "/project/delete-project/";
-export const DELETE_FILE_URL = BASE_URL + "/project/delete-file";
+export const UPLOAD_TO_SERVER_URL = BASE_URL + "/project/upload-file";
+export const GET_FILE_CONTENTS = BASE_URL + "/project/get-file-content";
